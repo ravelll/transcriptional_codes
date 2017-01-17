d6d574ecad2a9ae67422cc7dc72711ea05057563
ravelll <ravelll.taniguchi@gmail.com>
2017-01-17 22:34:13 +0900

    Add sample ToDo management application from WEB+DB Press vol92

---
+++ WEBDBvol92_todo/app/assets/javascripts/TaskApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class TaskApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="taskApp">
+        Hello, Web Development World!!
+      </div>
+    );
+  }
+}
