909366525f76e0f8dc2597b811a4821d72c24289
ravelll <ravelll.taniguchi@gmail.com>
2017-02-08 00:05:01 +0900

    Fix class to be more simple

--- guide_for_object_orient_designing/chapter6/bicycle.rb
+++ guide_for_object_orient_designing/chapter6/bicycle.rb
@@ -1,29 +1,9 @@
 class Bicycle
-  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
+  attr_reader :size, :chain, :tire_size
 
-  def initialize(args)
-    @style       = args[:style]
-    @size        = args[:size]
-    @tape_color  = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock  = args[:rear_shock]
+  def initialize(args = {})
+    @size = args[:size]
+    @chain = args[:chain]
+    @tire_size = args[:tire_size]
   end
-
-  def spares
-    if style == :road
-      {
-        chain: '10-speed',
-        tire_size: '23',
-        tape_color: tape_color,
-      }
-    else
-      {
-        chain: '10-speed',
-        tire_size: '2.1',
-        rear_shock: rear_shock,
-      }
-    end
-  end
-
-  # and other methods ...
 end
