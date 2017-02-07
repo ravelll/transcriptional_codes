f1b87d65a3f83f496ebdfe23ddd9c39172350ca8
ravelll <ravelll.taniguchi@gmail.com>
2017-02-07 23:24:43 +0900

    Add first bicycle class in chapter6

---
+++ guide_for_object_orient_designing/chapter6/bicycle.rb
@@ -0,0 +1,18 @@
+class Bicycle
+  attr_reader :size, :tape_color
+
+  def initialize(args)
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+  end
+
+  def spares
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color,
+    }
+  end
+
+  # and other methods ...
+end
